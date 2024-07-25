09f7f54c3825b8c3942513f2dd2ed5d6176ffc3e
jordanleewei <jordanleewei@gmail.com>
2024-07-25 15:31:31 +0800

    safe space so dont overlap with phone toolbar

--- lib/wardrobe/wardrobe_page.dart
+++ lib/wardrobe/wardrobe_page.dart
@@ -6,68 +6,70 @@ class WardrobePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: Center(
-                  child: Text('This is the Wardrobe Page'),
-                ),
-              ),
-              // Add other widgets here if necessary
-            ],
-          ),
-          Positioned(
-            top: 16.0,
-            left: 16.0,
-            right: 16.0,
-            child: Row(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                GestureDetector(
-                  onTap: () {
-                    // Add action you want to perform when the image is tapped
-                    print('Image tapped');
-                  },
-                  child: Image.asset(
-                    'lib/assets/KagaMe.png',
-                    width: 120.0,
-                    height: 90.0,
+                Expanded(
+                  child: Center(
+                    child: Text('This is the Wardrobe Page'),
                   ),
                 ),
-                SizedBox(width: 16.0), // Space between the image and the search bar
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30.0),
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                      ),
+                // Add other widgets here if necessary
+              ],
+            ),
+            Positioned(
+              top: 0,
+              left: 12.0,
+              right: 12.0,
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      // Add action you want to perform when the image is tapped
+                      print('Image tapped');
+                    },
+                    child: Image.asset(
+                      'lib/assets/KagaMe.png',
+                      width: 120.0,
+                      height: 60.0,
                     ),
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search Wardrobe',
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
-                        suffixIcon: IconButton(
-                          icon: Icon(Icons.filter_list, color: Colors.grey),
-                          onPressed: () {
-                            // Add filter action here
-                            print('Filter icon tapped');
-                          },
+                  ),
+                  SizedBox(width: 16.0), // Space between the image and the search bar
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30.0),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search Wardrobe',
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search, color: Colors.grey),
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.filter_list, color: Colors.grey),
+                            onPressed: () {
+                              // Add filter action here
+                              print('Filter icon tapped');
+                            },
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
