bc70931538e41a9f2780087994f51593ff26a8ff
Chien Shyong Lee <chienshyong_lee@mymail.sutd.edu.sg>
2025-03-04 18:58:54 +0800

    wardrobe recommend

--- lib/wardrobe/recommend_page.dart
+++ lib/wardrobe/recommend_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 // import 'package:go_router/go_router.dart';
 import '../services/auth_service.dart';
+import '../shop/product_detail_page.dart'; 
 
 class RecommendPage extends StatefulWidget {
   final String id;
@@ -14,50 +15,22 @@ class RecommendPage extends StatefulWidget {
 
 class _RecommendPageState extends State<RecommendPage> {
   final AuthService authService = AuthService();
-  bool isLoading = false;
+  bool isLoading = true;
 
-  // Dummy image list for the infinite scroll
-  List<String> _items = List.generate(10, (index) => 'Item $index');
+  //Placeholder while API is called
+  Map<String, dynamic> this_item_jsonResponse = {'image_url': 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg', 'category': '', 'color': '', 'name': ''};
+  List<Map<String, dynamic>> recommended = [];
 
-  // Scroll controller to detect when the user scrolls to the bottom
-  ScrollController _scrollController = ScrollController();
-
-  //Placeholders while API is called
-  Map<String, dynamic> jsonResponse = {'image_url': 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg', 'category': '', 'color': '', 'name': ''};
+  String prompt = "";
 
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(_onScroll);
-    fetchItemFromApi();
+    fetchThisItemFromApi();
+    fetchRecommendationsFromApi();
   }
 
-    // Fetch more items when scrolled to the bottom
-  void _fetchMore() async {
-    if (!isLoading) {
-      setState(() {
-        isLoading = true;
-      });
-
-      // Simulate a delay for loading more items (replace with API call)
-      await Future.delayed(Duration(seconds: 2));
-
-      setState(() {
-        isLoading = false;
-        _items.addAll(List.generate(10, (index) => 'Item ${_items.length + index}'));
-      });
-    }
-  }
-
-  // Detect when the user has scrolled to the bottom
-  void _onScroll() {
-    if (_scrollController.position.pixels ==
-        _scrollController.position.maxScrollExtent) {
-      _fetchMore();
-    }
-  }
-
-  Future<void> fetchItemFromApi() async {
+  Future<void> fetchThisItemFromApi() async {
     final String baseUrl = authService.baseUrl;
     final token = await authService.getToken();
     final request = http.MultipartRequest(
@@ -72,14 +45,41 @@ class _RecommendPageState extends State<RecommendPage> {
       if (response.statusCode == 200) {
         final responseBody = await response.stream.bytesToString(); // Convert response to string
         setState(() {
-          jsonResponse = json.decode(responseBody); // Decode JSON data as Map
-          isLoading = false;
+          this_item_jsonResponse = json.decode(responseBody); // Decode JSON data as Map
         });
       } else {
         throw Exception('Failed to load image');
       }
     } catch (error) {
       print('Error fetching image: $error');
+    }
+  }
+
+  Future<void> fetchRecommendationsFromApi() async {
+    final String baseUrl = authService.baseUrl;
+    final token = await authService.getToken();
+    final request = http.MultipartRequest(
+      'GET',
+      Uri.parse('$baseUrl/wardrobe/wardrobe_recommendation?_id=${widget.id}&additional_prompt=${prompt}'),
+    );
+    request.headers['Authorization'] = 'Bearer $token';
+
+    try {
+      final response = await request.send();
+
+      if (response.statusCode == 200) {
+        final responseBody = await response.stream.bytesToString(); // Convert response to string
+        final List<Map<String, dynamic>> data = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+        setState(() {
+          recommended = data;
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load recommend');
+      }
+    } catch (error) {
+      print('Error fetching recommend: $error');
       setState(() {
         isLoading = false;
       });
@@ -101,7 +101,7 @@ class _RecommendPageState extends State<RecommendPage> {
             child: Row(
               children: [
                 Image.network(
-                  jsonResponse['image_url'],
+                  this_item_jsonResponse['image_url'],
                   width: 200,
                   height: 200,
                 ),
@@ -131,186 +131,58 @@ class _RecommendPageState extends State<RecommendPage> {
             ],
           ),
 
-          SizedBox(height: 16),
-
-          Expanded(
-            child: TabbedPage()
-            ),
-
-          // // Infinite scroll grid section
-          // Expanded(
-          //   child: GridView.builder(
-          //     controller: _scrollController,
-          //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          //       crossAxisCount: 2,
-          //       crossAxisSpacing: 8.0,
-          //       mainAxisSpacing: 8.0,
-          //       childAspectRatio: 0.8,
-          //     ),
-          //     itemCount: _items.length + 1,
-          //     itemBuilder: (context, index) {
-          //       if (index == _items.length) {
-          //         return isLoading
-          //             ? Center(child: CircularProgressIndicator())
-          //             : SizedBox(); // Show loading indicator when loading
-          //       }
-
-          //       // Replace with your image network URL for real items
-          //       return Column(
-          //         children: [
-          //           Expanded(
-          //             child: Container(
-          //               decoration: BoxDecoration(
-          //                 image: DecorationImage(
-          //                   image: NetworkImage(
-          //                       'https://dummyimage.com/200x300/000/fff&text=${_items[index]}'), // Placeholder
-          //                   fit: BoxFit.cover,
-          //                 ),
-          //                 borderRadius: BorderRadius.circular(10),
-          //               ),
-          //             ),
-          //           ),
-          //         ],
-          //       );
-          //     },
-          //   ),
-          // ),
+          //Recommendations
+          isLoading
+            ? Center(child: CircularProgressIndicator())
+            : Container(
+              height: 250,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: recommended.length,
+                itemBuilder: (context, index) {
+                  final recommendedProduct = recommended[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProductDetailPage(product: recommendedProduct),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 150,
+                      margin: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            recommendedProduct['url'] ?? recommendedProduct['image_url'],
+                            width: 150,
+                            height: 150,
+                            fit: BoxFit.cover,
+                            errorBuilder: (context, error, stackTrace) => Icon(Icons.error),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            recommendedProduct['label'] ?? recommendedProduct['name'] ?? "",
+                            style: TextStyle(fontSize: 16),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Text(
+                            '\$${recommendedProduct['price']}',
+                            style: TextStyle(fontSize: 16, color: Colors.green),
+                          ),
+                        ],
+                      )
+                    )
+                  );
+                }
+              )
+            )
         ],
-      ),
-    );
-  }
-}
-
-class TabbedPage extends StatefulWidget {
-  @override
-  _TabbedPageState createState() => _TabbedPageState();
-}
-
-class _TabbedPageState extends State<TabbedPage> {
-  final AuthService authService = AuthService();
-  bool isLoading = false;
-
-  // Dummy image list for the infinite scroll
-  List<String> _items = List.generate(10, (index) => 'Item $index');
-
-  // Scroll controller to detect when the user scrolls to the bottom
-  ScrollController _scrollController = ScrollController();
-
-  //Placeholders while API is called
-  Map<String, dynamic> jsonResponse = {'image_url': 'https://craftsnippets.com/articles_images/placeholder/placeholder.jpg', 'category': '', 'color': '', 'name': ''};
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // Number of tabs
-      child: Column(
-        children: [
-          TabBar(
-            tabs: [
-              Tab(text: "From Partner Brands"),
-              Tab(text: "From Your Wardrobe"),
-            ],
-          ),
-
-      
-        Expanded(child: TabBarView(
-          children: [
-            Center( // From Partner Brands Tab
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Recommendations with items from our partner brands"),
-                  Expanded(
-                    child: GridView.builder(
-                      controller: _scrollController,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 8.0,
-                        mainAxisSpacing: 8.0,
-                        childAspectRatio: 0.8,
-                      ),
-                      itemCount: _items.length + 1,
-                      itemBuilder: (context, index) {
-                        if (index == _items.length) {
-                          return isLoading
-                              ? Center(child: CircularProgressIndicator())
-                              : SizedBox(); // Show loading indicator when loading
-                        }
-
-                        // Replace with your image network URL for real items
-                        return Column(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        'https://dummyimage.com/200x300/000/fff&text=${_items[index]}'), // Placeholder
-                                    fit: BoxFit.cover,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              Center( // From Your Wardrobe Tab
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Recommendations with items from your personal wardrobe"),
-                  Expanded(
-                    child: GridView.builder(
-                      controller: _scrollController,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 8.0,
-                        mainAxisSpacing: 8.0,
-                        childAspectRatio: 0.8,
-                      ),
-                      itemCount: _items.length + 1,
-                      itemBuilder: (context, index) {
-                        if (index == _items.length) {
-                          return isLoading
-                              ? Center(child: CircularProgressIndicator())
-                              : SizedBox(); // Show loading indicator when loading
-                        }
-
-                        // Replace with your image network URL for real items
-                        return Column(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                        'https://dummyimage.com/200x300/000/fff&text=${_items[index]}'), // Placeholder
-                                    fit: BoxFit.cover,
-                                  ),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        ],
-      ),
+      )
     );
   }
 }
